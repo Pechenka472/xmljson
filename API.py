858f9f27218081f31d1555338290f7c2824e28c0
Pechenka472 <andreymorozov007@gmail.com>
2021-12-05 17:13:12 +0500

    Поправил

--- API.py
+++ API.py
@@ -2,8 +2,8 @@ from itertools import groupby
 from urllib.request import urlopen
 from json import loads
 
-url = 'https://ru.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvlimit=500&titles=%D0%91%D0%B5%D0%BB%D1%8C%D0%BC%D0%BE%D0%BD%D0%B4%D0%BE%2C_%D0%96%D0%B0%D0%BD-%D0%9F%D0%BE%D0%BB%D1%8C'
-data = loads(urlopen(url).read())
+url = 'https://ru.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvlimit=500&titles=%D0%93%D1%80%D0%B0%D0%B4%D1%81%D0%BA%D0%B8%D0%B9,_%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80_%D0%91%D0%BE%D1%80%D0%B8%D1%81%D0%BE%D0%B2%D0%B8%D1%87'
+data = loads(urlopen(url).read().decode('utf8'))
 
-for i, j in groupby(data['query']['pages']['192203']['revisions'], key=lambda x: x['timestamp'][0:10]):
+for i, j in groupby(data['query']['pages']['183903']['revisions'], key=lambda x: x['timestamp'][0:10]):
     print(i, sum(1 for _ in j))
