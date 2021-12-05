a44aa7fc1e99a4c57444c8e9e0c12c907ff328d0
Pechenka472 <andreymorozov007@gmail.com>
2021-12-05 14:49:30 +0500

    1

---
+++ JSON Converter.py
@@ -0,0 +1,14 @@
+import xml.etree.ElementTree as ET
+from urllib.request import urlopen
+import json
+
+data = urlopen('https://lenta.ru/rss').read().decode('utf8')
+root = ET.fromstring(data)
+data_dict = []
+
+for i in root[0].findall('item'):
+    data_dict.append({'pubDate': i.find("pubDate").text, 'title': i.find('title').text})
+
+with open('news.json', 'wb') as json_file:
+    file = json.dumps(data_dict, indent=4, ensure_ascii=False).encode('utf8')
+    json_file.write(file)
